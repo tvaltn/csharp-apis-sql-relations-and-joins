2b1cca2a6ca3648260e35ab9006e6b2d89f54e3c
tvaltn <115498434+tvaltn@users.noreply.github.com>
2024-09-04 09:47:26 +0200

    Update initQueries.sql

--- initQueries.sql
+++ initQueries.sql
@@ -1,14 +1,3 @@
-CREATE TABLE films(
-	id SERIAL PRIMARY KEY,
-	title TEXT NOT NULL,
-	year INT NOT NULL,
-	genre TEXT NOT NULL,
-	score INT NOT NULL,
-	directorId INT REFERENCES directors(id),
-	starId INT REFERENCES stars(id),
-	writerId INT REFERENCES writers(id)
-);
-
 CREATE TABLE directors(
 	id SERIAL PRIMARY KEY,
 	name TEXT NOT NULL,
@@ -27,18 +16,29 @@ CREATE TABLE writers(
 	email TEXT NOT NULL
 );
 
+CREATE TABLE films(
+	id SERIAL PRIMARY KEY,
+	title TEXT NOT NULL,
+	year INT NOT NULL,
+	genre TEXT NOT NULL,
+	score INT NOT NULL,
+	directorId INT REFERENCES directors(id),
+	starId INT REFERENCES stars(id),
+	writerId INT REFERENCES writers(id)
+);
+
 INSERT INTO stars (name, birthdate)
 VALUES
-	('Keir Dullea', 1936/05/30),
-	('Mark Hamill', 1951/09/25),
-	('Gregory Peck', 1916/04/05),
-	('Leonardo DiCaprio', 1974/11/11),
-	('Julie Christie', 1940/04/14),
-	('Charlton Heston', 1923/10/04),
-	('Manos Katrakis', 1908/08/14),
-	('Rutger Hauer', 1944/01/23),
-	('Juliette Binoche', 1964/03/09),
-	('Gerard Depardieu', 1948/12/27)
+	('Keir Dullea', '1936/05/30'),
+	('Mark Hamill', '1951/09/25'),
+	('Gregory Peck', '1916/04/05'),
+	('Leonardo DiCaprio', '1974/11/11'),
+	('Julie Christie', '1940/04/14'),
+	('Charlton Heston', '1923/10/04'),
+	('Manos Katrakis', '1908/08/14'),
+	('Rutger Hauer', '1944/01/23'),
+	('Juliette Binoche', '1964/03/09'),
+	('Gerard Depardieu', '1948/12/27')
 
 INSERT INTO directors (name, country)
 VALUES
